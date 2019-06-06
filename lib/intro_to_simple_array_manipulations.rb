7e1b8467ce4645b1c398af3859255c4088db357e
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-06 20:14:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,5 +20,11 @@ def using_concat(arg1, arg2)
   arg1.concat(arg2)
 end
 def using_insert(array, element)
-array.insert(4, element)
+  array.insert(4, element)
+end
+def using_uniq(array)
+  array.uniq
+end
+def using_flatten(array)
+  array.flatten
 end
