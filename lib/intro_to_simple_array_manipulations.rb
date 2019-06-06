79d9c78b6caaface8d8d6565af2dd302551cb418
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-06 20:08:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,14 @@
 def using_push(array, element)
   array.push element
 end
+def using_unshift(array, element)
+  array.unshift element
+end
+def using_pop(array)
+  array.pop 
+  array
+end
+def pop_with_args(array)
+  array.pop(2)
+  array
+end
