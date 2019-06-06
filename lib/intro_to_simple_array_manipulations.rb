523d67fb75f71790404873fa5be06a5e65658abf
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-06 20:10:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,17 +6,13 @@ def using_unshift(array, element)
 end
 def using_pop(array)
   array.pop 
-  return array
 end
 def pop_with_args(array)
   array.pop(2)
-  return array
 end
 def using_shift(array)
   array.shift 
-  return array
 end
 def shift_with_args(array)
   array.shift(2)
-  return array
 end
